55e3ae17eb708ae1a2f70ed2378e5d32c005b5c0
petarperin <46424554+petarperin@users.noreply.github.com>
2019-01-09 00:57:54 +0100

    Update FibHeap.h

--- FibHeap.h
+++ FibHeap.h
@@ -1,23 +1,32 @@
 #pragma once
+#include <iostream>
+
+using namespace std;
+
+struct node
+{
+	node *p, *c, *left, *right;
+	int key;
+	int deg;
+	bool mark;
+};
+
 class FibHeap
 {
-	struct node
-	{
-		node *p, *c, *left, *right;
-		int key;
-		int deg;
-		bool mark;
-	};
+private:
 	int n;
 	node *ptr;
 	bool min;
-	
+	friend node* make_node(int key);
+
 
 public:
 	void init();
+	void print();
 	int get();
-	void unite(FibHeap h, int &steps);
+	void unite(FibHeap &h, int &steps);
 	void add(int elem, int &steps);
+	void destroy();
 	FibHeap();
 	~FibHeap();
 };
